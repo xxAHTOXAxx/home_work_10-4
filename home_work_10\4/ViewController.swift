8af0a13539ea1a5fb14e2a3a887bdc847fdf464f
Anton <xxahtoxaxx2015@gmail.com>
2023-06-01 23:56:58 +0400

    Initial Commit

---
+++ home_work_10\4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  home_work_10\4
+//
+//  Created by Антон Исаков on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
