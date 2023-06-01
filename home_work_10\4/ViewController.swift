a25b64e60ba49b64c2cfe6b5899984916924590f
Anton <xxahtoxaxx2015@gmail.com>
2023-06-02 00:07:26 +0400

    bug-fix
    удалены комментарии

--- home_work_10\4/ViewController.swift
+++ home_work_10\4/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  home_work_10\4
-//
-//  Created by Антон Исаков on 01.06.2023.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
